f205aa765aff3d8af1f9bcb0ce4472cc1506438e
Léandre <leandre.gaden@gmail.com>
2020-12-09 22:40:50 +0100

    ajout sessions

--- web/index.php
+++ web/index.php
@@ -12,35 +12,41 @@ $_SESSION['age'] = '24' ;
 
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>The Tickets</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <link rel="shortcut icon" href="web/favicon.ico" />
+    <link rel="stylesheet" type="text/css" href="css/style.css" />
+    <meta name="robots" content="noindex" />
 </head>
 <body>
-    
-<p>
-        Salut <?php $_SESSION['user'] ?> ! Tu es à l'accueil de mon site (index.php). Tu veux aller sur une autre page ?
+    <img src="images/the_tickets.png" alt="Logo de The TheTickets" class="logo" href="index.html">
+        <ul>
+            <li class="select"><a href="index.html">Page d'Accueil</a></li>
+            <li><a href="visu/organisation.html">Les évènements</a></li>
+            <li><a href="visu/connexion.html">Inscription/ Connection</a></a></li>
+            <li><a href="visu/paiement.html">Le Paiement</a></li>
+            <li><a href="visu/qrcode.html">Qr code page</a></li>
+        </ul>
+        <p>
+        <?php 
+        
+        if ($_SESSION['user'] === 'user' && $_SESSION['mot_de_passe'] === 'mdp') {
+            echo "Bonsoir " . $_SESSION['user'] . "! Bienvenue sur Thetickets.fr !";
+        } else {
+            echo "Bonsoir ! Veuillez vous connectez pour accéder a votre compte utilisateur.";
+        }
+        ?>
     </p>
-
-    <ul>
-        <li class="select"><a href="index.html">Page d'Accueil</a></li>
-        <li><a href="visu/organisation.html">Les évènements</a></li>
-        <li><a href="visu/connexion.html">Inscription/ Connection</a></a></li>
-        <li><a href="visu/paiement.html">Le Paiement</a></li>
-        <li><a href="visu/qrcode.html">Qr code page</a></li>
-        <link rel="stylesheet" href="css/style.css">
-    </ul>
-
-    <div id="page">
-        <div id="header" class="header">
-            <h1>
-                <a href="index.html">
-                    <img src="images/the_tickets.png" alt="Logo de The TheTickets" class="logo" href="index.html">
-                </a>
-            </h1>
+    <h1>
+        <div id="page">
+            <div id="header" class="header">
+        <a href="index.html">
+            <img src="images/image_concert.webp" alt="image de concert" class="concert" href="index.html">
+        </a>
         </div>
     </div>
 </body>
