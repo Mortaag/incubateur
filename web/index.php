84e3a62326d342b713b5ebe75a93cf0aa6a68d36
Léandre <leandre.gaden@gmail.com>
2020-12-10 11:46:59 +0100

    modif

--- web/index.php
+++ web/index.php
@@ -23,10 +23,10 @@ $_SESSION['age'] = '24' ;
     <meta name="robots" content="noindex" />
 </head>
 <body>
-    <img src="images/the_tickets.png" alt="Logo de The TheTickets" class="logo" href="index.html">
+    <img src="images/the_tickets.png" alt="Logo de The TheTickets" class="logo" href="index.php">
         <ul>
             <li class="select"><a href="index.php">Page d'Accueil</a></li>
-            <li><a href="visu/organisation.html">Les évènements</a></li>
+            <li><a href="visu/organisation.php">Les évènements</a></li>
             <li> <?php
             if ($_SESSION['user'] === 'user' && $_SESSION['mot_de_passe'] === 'mdp') {
                 echo "<html> <li> <a href='visu/interface-user.php'> Votre compte</a> </li> </html>";
@@ -36,8 +36,8 @@ $_SESSION['age'] = '24' ;
             ?> 
             
             <a href="visu/interface-user.php">Votre compte</a></a></li>
-            <li><a href="visu/paiement.html">Le Paiement</a></li>
-            <li><a href="visu/qrcode.html">Qr code page</a></li>
+            <li><a href="visu/paiement.php">Le Paiement</a></li>
+            <li><a href="visu/qrcode.php">Qr code page</a></li>
         </ul>
         <p>
         <?php 
@@ -54,8 +54,8 @@ $_SESSION['age'] = '24' ;
     <h1>
         <div id="page">
             <div id="header" class="header">
-        <a href="index.html">
-            <img src="images/image_concert.webp" alt="image de concert" class="concert" href="index.html">
+        <a href="index.php">
+            <img src="images/image_concert.webp" alt="image de concert" class="concert" href="index.php">
         </a>
         </div>
     </div>
