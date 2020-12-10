ab25d54eccbf85ae41ce9e80c11a023fec0aff0f
alicepetitjean <alice.petitjean55@orange.fr>
2020-12-10 09:48:23 +0100

    modif connexion.sql

--- connexion.sql
+++ connexion.sql
@@ -1,3 +1,10 @@
 drop database if exists connexion_user;
 create database connexion_user;
 use connexion_user;
+
+CREATE TABLE `session` (
+  `sess_id` char(40) NOT NULL,
+  `sess_datas` text NOT NULL,
+  `sess_expire` bigint(20) NOT NULL,
+  UNIQUE KEY `sess_id` (`sess_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
