f04e839dd17cb7ce0f7fcacb97384bf7ec4d9dda
alicepetitjean <alice.petitjean55@orange.fr>
2021-01-19 14:54:06 +0100

    etape 2

--- new/achatterminer.php
+++ new/achatterminer.php
@@ -37,7 +37,7 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="../../assets/js/vendor/popper.min.js"></script>
     <script src="../../dist/js/bootstrap.min.js"></script>
-    <button type="submit" class="proceed-btn"><a href="generate.php">Accès au QRCode</a></button>
+    <button type="submit" class="proceed-btn"><a href="qrcode.php">Accès au QRCode</a></button>
 </body>
 
 </html>
